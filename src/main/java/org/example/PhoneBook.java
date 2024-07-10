fb27a99c9700990fc9f99b19f745d208cb532d67
Nastya B <nastybondareva@yandex.ru>
2024-07-10 20:21:46 +0300

    add implemented

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,10 +1,18 @@
 package org.example;
 
+import java.util.TreeMap;
+
 public class PhoneBook {
-
+    static TreeMap<String, Long> phoneBookMap = new TreeMap<>();
     public static int add(String name, long number ) {
-
-        return 0;
+        if (phoneBookMap.containsKey(name)) {
+            throw new IllegalArgumentException("Данное имя уже существует, введите другое имя");
+        } else {
+            phoneBookMap.put(name, number);
+        }
+        return phoneBookMap.size();
     }
+
+
 
 }
